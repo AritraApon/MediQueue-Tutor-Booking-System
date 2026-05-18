a4d3371e6a05651dbfb01dafe0bf212deeef25ec
AritraApon <aritromazumdar8@gmail.com>
2026-05-19 00:08:52 +0600

    add tutor from desing

--- src/Components/Share/Navbar.jsx
+++ src/Components/Share/Navbar.jsx
@@ -72,7 +72,7 @@ const Navbar = () => {
                 <div className="flex items-center gap-2">
 
                     <Link href="/" className="flex items-center gap-2">
-                        <Image src={'/logo.png'} alt='logo' width={35} height={35} />
+                        
                         <h1 className="text-xl font-bold bg-gradient-to-r from-blue-600 to-indigo-500 bg-clip-text text-transparent">
                             MediQueue
                         </h1>

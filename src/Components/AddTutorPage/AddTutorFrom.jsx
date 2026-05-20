ca712ad29d8682f6b60778db3b720dc626edefb1
AritraApon <aritromazumdar8@gmail.com>
2026-05-20 21:08:00 +0600

    gravity icon problem

--- src/Components/AddTutorPage/AddTutorFrom.jsx
+++ src/Components/AddTutorPage/AddTutorFrom.jsx
@@ -38,7 +38,7 @@ const AddTutorFrom = () => {
     // console.log("Sending Data:", addTutorData);
       const {data:tokenData} = await authClient.token()
     try {
-        const res = await fetch(`${process.env.}/tutors`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tutors`, {
             method: 'POST',
             headers: {
                 'content-type': 'application/json',
